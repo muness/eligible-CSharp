924a97670a4e0cda2566c880e52b9e1cfefc5918
balu-eligible <balu@eligible.com>
2016-05-24 16:56:43 +0530

    Fix for PR comments - added a period reference to refere the actual name of property

--- Eligible.NET/src/EligibleService.Model/Coverage/PreExistingCondition.cs
+++ Eligible.NET/src/EligibleService.Model/Coverage/PreExistingCondition.cs
@@ -23,9 +23,20 @@ namespace EligibleService.Model.Coverage
 
     public class WaitingPeriod
     {
+        /// <summary>
+        /// Period actual json property will be one of the following,
+        /// "", "discharge", "issue", "effective_fate_of_change", "period_start", "period_end", "completion",
+        /// "coordination_of_benefits","plan", "benefit", "primary_care_provider", "latest_visit_or_consultation",
+        /// "eligibilty", "added", "consolidated_omnibus_budget_reconciliation_begin", "consolidated_omnibus_budget_reconciliation_end",
+        /// "premium_paid_to_date_begin", "premium_paid_to_date_end", "plan_begin", "plan_end,benefit_begin",
+        /// "benefit_end", "eligibility_begin", "eligibility_end", "enrollment", "admission", "date_of_death",
+        /// "certification", "service","policy_effective", "policy_expiration", "date_of_last_update", "status"
+        /// </summary>
         public Period Period { get; set; }
+        public string PeriodRefers { get; set; }
         [JsonExtensionData]
         private IDictionary<string, JToken> _additionalData;
+
         List<string> dateLabels = new List<string>(
             new string[] {  "", "discharge", "issue", "effective_fate_of_change", "period_start", "period_end", "completion",
                             "coordination_of_benefits","plan", "benefit", "primary_care_provider", "latest_visit_or_consultation",
@@ -42,7 +53,10 @@ namespace EligibleService.Model.Coverage
             foreach (string dateLabel in dateLabels)
             {
                 if (payer_key.Contains(dateLabel))
+                {
                     key = dateLabel;
+                    PeriodRefers = key;
+                }
             }
             Period = JsonConvert.DeserializeObject<Period>(_additionalData[key].ToString());
         }
